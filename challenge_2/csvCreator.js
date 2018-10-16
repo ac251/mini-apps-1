52d323db08521638110f4a316c3867780797c9ad
ac251 <aaron.d.cohn@gmail.com>
2018-10-16 15:48:46 -0700

    App accepts JSON and sends back parsed to CSV as plain text.

--- challenge_2/csvCreator.js
+++ challenge_2/csvCreator.js
@@ -1,25 +1,47 @@
 
 
-module.exports = (obj, _depth = 0, _arrays = {}) => {
+module.exports = (obj) => {
   let csv = '';
   const arrays = [];
-  arrays[0] = [];
-  for (let key in obj) {
-    if (key !== 'children') {
-      arrays[0].push(key);
+  const queue = [obj];
+  let nextIdxFromQueue = 0;
+  let lastKeysIdx;
+  let doKeysMatch = (obj) => {
+    if (lastKeysIdx === undefined) { return false; }
+    for (let key in obj) {
+      if (key !== 'children' && !arrays[lastKeysIdx].has(key)) {
+        return false;
+      }
+    }
+    for (let key of arrays[lastKeysIdx].values()) {
+      if (!(key in obj)) {
+        return false;
+      }
+    }
+    return true;
+  }
+  while (nextIdxFromQueue < queue.length) {
+    let currentObj = queue[nextIdxFromQueue++];
+    if (!doKeysMatch(currentObj)) {
+      let newKeys = new Set();
+      for (let key in currentObj) {
+        if (key !== 'children') {
+          newKeys.add(key);
+        }
+      }
+      lastKeysIdx = arrays.length;
+      arrays.push(newKeys);
+    }
+    let values = []
+    arrays[lastKeysIdx].forEach(key => values.push(currentObj[key]))
+    arrays.push(values);
+    if (currentObj.children) {
+      currentObj.children.forEach(child => queue.push(child));
     }
   }
   
-  let recurser = (child = obj) => {
-    arrays.push(arrays[0].map(key => child[key]));
-    if (child.children) {
-      child.children.forEach(child => recurser(child));
-    }
-  };
-  
-  recurser();
-  
-  return arrays.map(subArr => subArr.join(',')).join('\n');
-  
-  
+  return arrays.map(array => Array.from(array))
+    .map(array => array.join(','))
+    .join('\n');
+ 
 };
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path')
+const csvCreator = require('./csvCreator.js')
 
 const app = express();
+app.use(express.urlencoded())
+app.use(express.json());
 
 
 app.get('/', (req, res) => {
   fs.readFile(path.join(__dirname, '/client/index.html'), 'utf-8', (err, data) => {
-    console.log(data);
     res.status(200).set('Content-type', 'text/html').send(data);
   });
+});
+
+app.post('/json', (req, res) => {
+  console.log(req.body);
+  res.status(201).set('Content-Type', 'text/plain').send(csvCreator(JSON.parse(req.body.mydata)));
 });
 
 
