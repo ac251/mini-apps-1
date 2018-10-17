440ff810671d656156adc95760fe2f64c6058bd4
ac251 <aaron.d.cohn@gmail.com>
2018-10-16 17:42:43 -0700

    Refactored CSV creator to index rows, keep track of each child's parent row.

--- challenge_2/csvCreator.js
+++ challenge_2/csvCreator.js
@@ -1,5 +1,58 @@
 
 
+// module.exports = (obj) => {
+//   if (typeof obj === 'string') {
+//     try {
+//       obj = JSON.parse(obj);
+//     } catch(err) {
+//       obj = {"error message" : "invalid input, try again"};
+//     }
+//   }
+//   let csv = '';
+//   const arrays = [];
+//   const queue = [obj];
+//   let nextIdxFromQueue = 0;
+//   let lastKeysIdx;
+//   let doKeysMatch = (obj) => {
+//     if (lastKeysIdx === undefined) { return false; }
+//     for (let key in obj) {
+//       if (key !== 'children' && !arrays[lastKeysIdx].has(key)) {
+//         return false;
+//       }
+//     }
+//     for (let key of arrays[lastKeysIdx].values()) {
+//       if (!(key in obj)) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+//   while (nextIdxFromQueue < queue.length) {
+//     let currentObj = queue[nextIdxFromQueue++];
+//     if (!doKeysMatch(currentObj)) {
+//       let newKeys = new Set();
+//       for (let key in currentObj) {
+//         if (key !== 'children') {
+//           newKeys.add(key);
+//         }
+//       }
+//       lastKeysIdx = arrays.length;
+//       arrays.push(newKeys);
+//     }
+//     let values = []
+//     arrays[lastKeysIdx].forEach(key => values.push(currentObj[key]))
+//     arrays.push(values);
+//     if (currentObj.children) {
+//       currentObj.children.forEach(child => queue.push(child));
+//     }
+//   }
+  
+//   return arrays.map(array => Array.from(array))
+//     .map(array => array.join(','))
+//     .join('<br/>');
+ 
+// };
+
 module.exports = (obj) => {
   if (typeof obj === 'string') {
     try {
@@ -8,47 +61,30 @@ module.exports = (obj) => {
       obj = {"error message" : "invalid input, try again"};
     }
   }
-  let csv = '';
   const arrays = [];
   const queue = [obj];
   let nextIdxFromQueue = 0;
-  let lastKeysIdx;
-  let doKeysMatch = (obj) => {
-    if (lastKeysIdx === undefined) { return false; }
-    for (let key in obj) {
-      if (key !== 'children' && !arrays[lastKeysIdx].has(key)) {
-        return false;
-      }
-    }
-    for (let key of arrays[lastKeysIdx].values()) {
-      if (!(key in obj)) {
-        return false;
-      }
-    }
-    return true;
-  }
-  while (nextIdxFromQueue < queue.length) {
-    let currentObj = queue[nextIdxFromQueue++];
-    if (!doKeysMatch(currentObj)) {
-      let newKeys = new Set();
-      for (let key in currentObj) {
-        if (key !== 'children') {
-          newKeys.add(key);
-        }
-      }
-      lastKeysIdx = arrays.length;
-      arrays.push(newKeys);
-    }
-    let values = []
-    arrays[lastKeysIdx].forEach(key => values.push(currentObj[key]))
-    arrays.push(values);
-    if (currentObj.children) {
-      currentObj.children.forEach(child => queue.push(child));
-    }
-  }
+  const keys = new Set(['index', 'parent'])
   
-  return arrays.map(array => Array.from(array))
-    .map(array => array.join(','))
-    .join('<br/>');
- 
+  while (nextIdxFromQueue < queue.length) {
+    let currentObj = queue[nextIdxFromQueue];
+    currentObj.index = nextIdxFromQueue;
+    for (let key in currentObj) {
+      if (key !== 'children') {
+        keys.add(key);
+      }
+    }
+    if (currentObj.children) {
+      currentObj.children.forEach(child => {
+        child.parent = nextIdxFromQueue;
+        queue.push(child);
+      });
+    }
+    nextIdxFromQueue++;
+  }
+  arrays[0] = Array.from(keys);
+  queue.forEach(obj => {
+    arrays.push(arrays[0].map(key => obj[key]));
+  });
+  return arrays.map(array => array.join(',')).join('<br/>');
 };
