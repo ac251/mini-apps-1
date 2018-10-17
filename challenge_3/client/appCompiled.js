5cce6e107a5fba5ea247548f709c0ba04f72750f
ac251 <aaron.d.cohn@gmail.com>
2018-10-17 12:29:28 -0700

    Set up babel, react transpiliation.

--- challenge_3/client/appCompiled.js
+++ challenge_3/client/appCompiled.js
@@ -3,15 +3,13 @@ import React from 'React';
 class App extends React.component {
   constructor(props) {
     super(props);
-    this.state = {
-      
-    }
+    this.state = {};
   }
-  
-  render() {
-    return (
-    <SpecialList name={this.state.name}></SpecialList>
-    )
-  }
-}
 
+  render() {
+    return React.createElement(SpecialList, {
+      name: this.state.name
+    });
+  }
+
+}
